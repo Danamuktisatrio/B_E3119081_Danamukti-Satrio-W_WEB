22f5e1e8575930eff86fa3e99a8827d7be65f265
Danamuktisatrio <66562244+Danamuktisatrio@users.noreply.github.com>
2021-03-15 23:29:11 +0700

    Minggu6

---
+++ Minggu6/application/models/tugas4_model.php
@@ -0,0 +1,11 @@
+<?php 
+class tugas4_model extends CI_Model {
+    public function get_data()
+	{
+        $data_mahasiswa = [
+			["nama"=>"Danamukti Satrio Wibowo","nim"=>"E31190851"],
+			 ];
+        return $data_mahasiswa;
+	}
+}
+?>
